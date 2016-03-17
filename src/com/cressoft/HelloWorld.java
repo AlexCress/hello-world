5e4919688f719d761c49c5c37e7767b412ea4163
AlexCress <alex_cress@hotmail.com>
2016-03-17 00:57:07 -0600

    Added code
    Added HelloWorld.java and project-related files (Eclipse).

---
+++ src/com/cressoft/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.cressoft;
+
+/**
+ * 
+ * @author Alex
+ *
+ * Everyone's got to start somewhere, and for programmers it is the Hello World program.
+ * Prints "Hello World" to the console. Nothing spectacular.
+ */
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World");
+	}
+}
